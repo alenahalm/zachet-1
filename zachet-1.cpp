ea0a8a6200c17560610f54f6fcc273cad95bac91
alenahalm <alenahalmatova749@gmail.com>
2020-12-26 10:06:46 +0800

    Initial commit

---
+++ zachet-1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	const int m = 5;
+	double z[m][m];
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> z[i][j];
+		}
+	}
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < m; j++) {
+			if (i != j) {
+				z[i][j] /= z[j][j];
+			}
+			cout << z[i][j] << " ";
+		}
+		cout << " " << endl;
+	}
+	for (int i = 0; i < m; i++) {
+		z[i][i] = 1;
+	}
+	system("pause");
+}
